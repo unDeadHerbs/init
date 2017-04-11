4e0a4c5a9984b224ddf6f2d142f5f1cf132c50fd
unDeadHerbs <undeadherbs@gmail.com>
2017-04-11 10:21:37 -0500

    enabled erc logging and dcc

--- init.el
+++ init.el
@@ -26,7 +26,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/todo"))))
+ '(erc-modules
+   (quote
+    (autojoin button completion dcc fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
+ '(org-agenda-files (quote ("~/todo")))
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
