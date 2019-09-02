8a31bc8f5bbd3369286670178a7b950e88e04031
Murray Fordyce <Murray.Fordyce@gmail.com>
2019-09-02 15:02:39 -0500

    in case of debian, build programs myself

---
+++ in_case_of_debian_break_glass.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# This script builds a clang, git, and emacs as user files and then removes them from the base system.  This is done because debian and its children (ubuntu) don't have updated versions of theses programs and I reliy on their "newer" (2 year old) features. (And backports (the "correct" solution) don't always exist.)
+
+mkdir ~/bin
+mkdir -p ~/build_dir
+
+##
+#  Install old Versions
+##
+sudo apt install git -y
+sudo apt install gcc -y
+
+##
+#  Install Clang
+##
+cd ~/build_dir
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+mkdir build
+cd build
+cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm
+make
+cd bin
+for file in $(ls)
+do
+    ln -sf ~/build_dir/llvm-project/build/bin/$file ~/bin
+done
+ln -sf ~/bin/clang ~/bin/cc
+sudo apt remove gcc -y
+sudo apt remove clang -y
+
+##
+#  Install Git
+##
+cd ~/build_dir
+git clone git@github.com:git/git
+cd git
+make
+ln -sf ~/build_dir/git/bin-wrappers/git ~/bin
+sudo apt remove git -y
+
+##
+#  Install Emacs
+##
+cd ~/build_dir
+git clone git://git.sv.gnu.org/emacs.git
+cd emacs
+git checkout emacs-26.3
+sudo apt install libxpm-dev libjpeg-dev libgif-dev libtiff-dev gnutls-dev
+./autogen.sh
+./configure --with-x-toolkit=no
+make
+ln -sf ~/build_dir/emacs/src/emacs ~/bin
+ln -sf ~/build_dir/emacs/lib-src/ctags ~/bin
+ln -sf ~/build_dir/emacs/lib-src/etags ~/bin
+ln -sf ~/build_dir/emacs/lib-src/emacsclient ~/bin
+sudo apt remove emacs -y
