1619ce888ee6cb37dac63dc188c68abb5ca754e6
Murray Fordyce <unDeadHerbs@gmail.com>
2019-09-04 14:36:57 -0500

    added some more build requierments for debian fix

--- in_case_of_debian_break_glass.sh
+++ in_case_of_debian_break_glass.sh
@@ -2,18 +2,21 @@
 
 # This script builds a clang, git, and emacs as user files and then removes them from the base system.  This is done because debian and its children (ubuntu) don't have updated versions of theses programs and I reliy on their "newer" (2 year old) features. (And backports (the "correct" solution) don't always exist.)
 
-mkdir ~/bin
+mkdir -p ~/bin
+export PATH="$HOME/bin:$PATH"
 mkdir -p ~/build_dir
 
 ##
 #  Install old Versions
 ##
-sudo apt install git -y
-sudo apt install gcc -y
+sudo apt install -y git
+sudo apt install -y gcc
 
 ##
 #  Install Clang
 ##
+if false
+then
 cd ~/build_dir
 git clone https://github.com/llvm/llvm-project.git
 cd llvm-project
@@ -29,6 +32,7 @@ done
 ln -sf ~/bin/clang ~/bin/cc
 sudo apt remove gcc -y
 sudo apt remove clang -y
+fi
 
 ##
 #  Install Git
@@ -36,6 +40,7 @@ sudo apt remove clang -y
 cd ~/build_dir
 git clone git@github.com:git/git
 cd git
+sudo apt install -y libssl-dev zlib1g-dev libcurl4-openssl-dev gettext libexpat1-dev
 make
 ln -sf ~/build_dir/git/bin-wrappers/git ~/bin
 sudo apt remove git -y
@@ -47,7 +52,7 @@ cd ~/build_dir
 git clone git://git.sv.gnu.org/emacs.git
 cd emacs
 git checkout emacs-26.3
-sudo apt install libxpm-dev libjpeg-dev libgif-dev libtiff-dev gnutls-dev
+sudo apt install -y libxpm-dev libjpeg-dev libgif-dev libtiff-dev gnutls-dev autoconf texinfo libpng-dev
 ./autogen.sh
 ./configure --with-x-toolkit=no
 make
