0e66bdbf96056be0113698a606584fa65da2c014
Murray Fordyce <undeadherbs@gmail.com>
2020-01-10 10:51:12 -0600

    Builds without %desktop-services

--- config.scm
+++ config.scm
@@ -2,9 +2,10 @@
 ;; by the graphical installer.
 
 (use-modules (gnu)
-	     (srfi srfi-1))  ;for 'remove'
+	     (srfi srfi-1) ;for 'remove'
+	     (gnu services sound))
 (use-package-modules shells)
-(use-service-modules desktop networking ssh xorg pm)
+(use-service-modules desktop networking ssh xorg pm avahi dbus)
 
 (operating-system
   (locale "en_US.utf8")
@@ -40,11 +41,23 @@
       %base-packages))
   (services
     (append
-      (list (service openssh-service-type)
-            (set-xorg-configuration
-              (xorg-configuration
-                (keyboard-layout keyboard-layout)))
-	    (service tlp-service-type))
-      (delq (list (service gdm-service-type))
-	      %desktop-services))))
+     (list (service openssh-service-type)
+	   ;; console-fonts
+	   (service network-manager-service-type)
+	   (service wpa-supplicant-service-type)
+	   (service usb-modeswitch-service-type)
+	   (service avahi-service-type)
+	   ;; upower ; from gnome, havn't renabled
+	   ;; (service accountsservice-service) ; TODO: What's this do?
+	   ;; colord ; from gnome, havn't renabled
+	   ;; (service geoclue-service)
+	   (service dbus-root-service-type)
+	   (service polkit-service-type)
+	   ;; (service polkit-wheel-service-type) ; can't find in repo
+	   (service elogind-service-type)
+	   ;; (service ntp-service-type)
+	   ;; x11-socket-directory
+	   (service alsa-service-type)
+	   (service tlp-service-type))
+     %base-services)))
        
