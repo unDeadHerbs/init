cb1d21a452bdbda0494de754674c35bddce56917
Murray Fordyce <unDeadHerbs@gmail.com>
2020-03-21 16:59:09 -0500

    Built initial windows config

---
+++ Windows_Install.ps1
@@ -0,0 +1,23 @@
+
+# If not Administrator
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    # Startscript as Administrator
+    Start-Process powershell.exe "-ExecutionPolicy Bypass -File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs -NoNewWindow -Wait
+    exit
+}
+
+# Install Chocolatey
+# From https://chocolatey.org/docs/installation#install-with-powershellexe
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Wraped to protect current shell
+Start-Process powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" -NoNewWindow -Wait
+
+# Install Utilities
+# choco install -y xming cmder git msys2
+# Wraped to protect current shell
+Start-Process powershell.exe "choco install -y xming cmder git msys2" -NoNewWindow -Wait
+
+# Import cmder config
+# ConEmu.xml
+Copy-Item Cmder_Config.xml C:\tools\Cmder\config\user-ConEmu.xml
