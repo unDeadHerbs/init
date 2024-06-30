78086bc4db7665b5a0efd9d5899ce7b31feb03c1
Murray Fordyce <unDeadHerbs@gmail.com>
2024-06-30 09:54:31 -0500

    22 current  2024-06-30 09:54:23  24.05.2150.89c49874fb15  6.6.35                          *

--- system_config/sia/sia.nix
+++ system_config/sia/sia.nix
@@ -159,7 +159,10 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.X11Forwarding = true;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
