e13893c4cbb0215bf504114b1a0e1b13c56569cc
Murray Fordyce <unDeadHerbs@gmail.com>
2024-06-29 21:57:05 -0500

    update the install script to use stow

--- install.sh
+++ install.sh
@@ -2,80 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 
-#################
-# link dotfiles #
-#################
-
-backup_increment(){
-    # USAGE: backup_increment file
-    # Creates a .bak file for the file also backs up any backup that
-    # would be overwritten
-    [[ -e "$1.bak" ]] && backup_increment "$1.bak"
-    mv "$1" "$1.bak"
-}
-
-backup_then_link(){
-    # USAGE: backup_then_link source dest
-    # creates a backup of the destination if needed then links the
-    # source to the destination
-
-    if [[ -e "$2" ]]
-    then
-	if [[ -L "$2" ]]
-	then
-	    rm "$2" # it was a link
-	    ln -sf "$1" "$2"
-	elif [[ -d "$2" ]]
-	then
-	    # it's a directory
-	    if [[ -d "$1" ]]
-	    then
-		# for each in $1 try in $2
-		{ cd "$1" ; ls -DA ; } |
-		    grep -v "[#~]" | # ignore temp files
-		    grep -v '^[.]git$' | # ignore the fact that this is a git repo
-		    egrep -v '^[.]+$' | # ignore . and ..
-		    while read -r file
-		    do
-			backup_then_link "$1/$file" "$2/$file"
-		    done
-	    else
-		backup_increment "$2"
-		ln -sf "$1" "$2"
-	    fi
-	elif diff -qw "$1" "$2" | grep "." > /dev/null
-	then
-	    # same content
-	    # We've already checked that $2 isn't a link and rm won't
-	    # follow sub simlinks. Please don't hardlink things,
-	    # particularly in common locations.
-	    rm -rf "$2"
-	    ln -sf "$1" "$2"
-	else
-	    backup_increment "$2"
-	    ln -sf "$1" "$2"
-	fi
-    else
-	ln -sf "$1" "$2"
-    fi
-}
-
-#for file in $( { cd $DIR ; ls -d .* } | grep -v "[#~]" | grep -v "^.git$" )
-#{ cd $DIR ; ls -d .* ; } |
-#    grep -v "[#~]" | # ignore temp files
-#    grep -v '^[.]git$' | # ignore the fact that this is a git repo
-#    egrep -v '^[.]+$' | # ignore . and ..
-#    while read -r file
-#    do
-#	backup_then_link "$DIR/$file" "$HOME/$file"
-#    done
-backup_then_link "$DIR/home" "$HOME"
-
-###################################
-# check for reqs and setup things #
-###################################
-
 toInstall=""
+
+if type stow > /dev/null 2>&1
+then
+		pushd home
+		stow . -v -t ~
+		popd
+else
+		toInstall="$toInstall stow"		
+fi
 
 if type git > /dev/null 2>&1
 then
