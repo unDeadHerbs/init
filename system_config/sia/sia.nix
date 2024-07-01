2c82146e47fb06a38312e117c04c553b2b92cfdc
Murray Fordyce <unDeadHerbs@gmail.com>
2024-07-01 09:16:40 -0500

    37 current  2024-07-01 09:16:28  24.05.2310.7dca15289a1c  6.6.36                          *

--- system_config/sia/sia.nix
+++ system_config/sia/sia.nix
@@ -102,11 +102,13 @@
       "google-chrome"
     ];
 
+  programs.zsh.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.udh = {
     isNormalUser = true;
     description = "udh";
     extraGroups = ["networkmanager" "wheel"];
+    shell = pkgs.zsh;
     packages = with pkgs; [
       #moreutils cpufrequtils binutils usbutils
       #sh-z sicp inetutils xpdf
