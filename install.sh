f346a1a3f7a138e5fb14894aa0968c8c1b84eba4
Murray Fordyce <unDeadHerbs@gmail.com>
2024-07-05 11:45:59 -0500

    fix install script indenting

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ toInstall=""
 
 if type stow > /dev/null 2>&1
 then
-		pushd home
-		stow . -v -t ~
-		popd
+    pushd home
+    stow . -v -t ~
+    popd
 else
-		toInstall="$toInstall stow"		
+    toInstall="$toInstall stow"   
 fi
 
 if type git > /dev/null 2>&1
@@ -18,7 +18,7 @@ then
     git config --global include.path "$DIR/gitconfig"
     git config --global core.excludesFile "$DIR/gitignore"
     [[ "$(git config --global user.signingkey)" != "" ]] &&
-	git config --global commit.gpgsign true
+        git config --global commit.gpgsign true
     git config --global init.templatedir "$DIR/git_templates"
 else
     toInstall="$toInstall git"
