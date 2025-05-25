2e3331941518ad6f96ad1a1a6c85e6c6430b74ee
Murray Fordyce <unDeadHerbs@gmail.com>
2025-05-25 01:22:11 -0500

    192 current  2025-05-25 01:13:53  24.11.718140.2baa12ff6991  6.6.91                          *

--- system_config/sia/sia.nix
+++ system_config/sia/sia.nix
@@ -16,6 +16,18 @@ in {
   boot.supportedFilesystems = ["ntfs"];
   boot.loader.grub.splashImage = ../../bg/nix-wallpaper-mosaic-blue.png;
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  system.autoUpgrade = {
+    enable = true; # periodically execute systemd service nixos-upgrade.service
+    allowReboot = false; # If false, run nixos-rebuild switch --upgrade
+  };
   nix.gc = {
     automatic = true;
     dates = "weekly";
@@ -224,15 +236,4 @@ in {
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-  system.autoUpgrade.enable = true; # periodically execute systemd service nixos-upgrade.service
-  system.autoUpgrade.allowReboot = false; # If false, run nixos-rebuild switch --upgrade
 }
