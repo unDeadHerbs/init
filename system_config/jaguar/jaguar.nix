be7ede6bf84ff8c8b0ed60af599d5b77bcad24a4
Murray Fordyce <unDeadHerbs@gmail.com>
2025-05-25 15:11:49 -0500

    jaguar: setup remote admin

--- system_config/jaguar/jaguar.nix
+++ system_config/jaguar/jaguar.nix
@@ -20,6 +20,16 @@ in {
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  system.autoUpgrade.enable = true; # periodically execute systemd service nixos-upgrade.service
+  system.autoUpgrade.allowReboot = false; # If false, run nixos-rebuild switch --upgrade
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -157,26 +167,31 @@ in {
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
+  # Setup Remote Administration
   services.openssh = {
     enable = true;
     settings.X11Forwarding = true;
+    settings.PasswordAuthentication = false;
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-  system.autoUpgrade.enable = true; # periodically execute systemd service nixos-upgrade.service
-  system.autoUpgrade.allowReboot = false; # If false, run nixos-rebuild switch --upgrade
+  services.tor = {
+    enable = true;
+    openFirewall = true;
+    relay = {
+      enable = true;
+      role = "relay";
+      onionServices.remoteAdmin = {
+        version = 3;
+        map = [
+          {
+            port = 1234;
+            target = {
+              addr = "[::1]";
+              port = 22;
+            };
+          }
+        ];
+      };
+    };
+  };
 }
