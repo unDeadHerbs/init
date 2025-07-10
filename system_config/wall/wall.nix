822b5e66ac34d774a0616a339846b2a7d79ba40a
Murray Fordyce <unDeadHerbs@gmail.com>
2025-07-10 17:25:18 -0500

    wall: move from rpi to x200

--- system_config/wall/wall.nix
+++ system_config/wall/wall.nix
@@ -1,34 +1,62 @@
 # Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
-
-  # The Pi is a bit small, so tasks taking a while isn't concerning.
-  boot.kernel.sysctl = {
-    "kernel.hung_task_timeout_secs" = 0;
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  # sudo nix-channel --add https://channels.nixos.org/nixos-unstable nixos-unstable
+  # sudo nix-channel --update
+  unstable = import <nixos-unstable> {config = {allowUnfree = true;};};
+in {
+  # Bootloader.
+  boot = {
+    loader.grub.enable = true;
+    loader.grub.device = "/dev/sda";
+    loader.grub.useOSProber = true;
   };
-  
-  networking.hostName = "wall"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  swapDevices = [{
-    device = "/swapfile";
-    size = 4 * 1024; # 4GB
-  }];
-  
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  system.autoUpgrade.enable = true; # periodically execute systemd service nixos-upgrade.service
+  system.autoUpgrade.allowReboot = false; # If false, run nixos-rebuild switch --upgrade
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  services.avahi = {
+    enable = true;
+    publish.enable = true;
+    publish.workstation = true;
+    publish.userServices = true;
+  };
+
+  # Set your time zone.
   time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
+  #i18n.defaultLocale = "uk_UA.UTF-8";
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = ["uk_UA.UTF-8/UTF-8" "ru_RU.UTF-8/UTF-8" "en_US.UTF-8/UTF-8"];
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -40,54 +68,76 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-  
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    xrandrHeads = [
-      {
-        monitorConfig = "Option \"Rotate\" \"left\"";
-        output = "HDMI-1";
-        primary = true;
-      }
-    ];
-    xkb = {
-      layout = "us";
-      variant = "";
+
+  # Display
+  services = {
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      xrandrHeads = [
+        {
+          monitorConfig = "Option \"Rotate\" \"left\"";
+          output = "VGA-1";
+          primary = true;
+        }
+        {
+          monitorConfig = "Option \"Ignore\" \"true\"";
+          output = "LVDS-1";
+        }
+      ];
+      displayManager = {
+        #lightdm.enable = true;
+      };
+      windowManager.i3.enable = true;
+      #desktopManager.xfce.enable = true;
     };
     displayManager = {
-      lightdm.enable = true;
+      defaultSession = "none+i3";
+      autoLogin.enable = true;
+      autoLogin.user = "reiko";
     };
-    windowManager.i3.enable = true;
   };
-  services.displayManager = {
-    defaultSession = "none+i3";
-    autoLogin.enable = true;
-    autoLogin.user = "reiko";
-  };
+
+  fonts.packages = with pkgs; [
+    fira
+    fira-code
+  ];
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+  # Sound
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-  programs.zsh.enable = true;
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "google-chrome"
+    ];
+
   users.users.reiko = {
     isNormalUser = true;
-    description = "reiko";
-    extraGroups = ["networkmanager" "wheel"];
+    description = "Reiko";
+    extraGroups = ["networkmanager" "disk" "audio" "dialout" "video" "input" "wheel"];
     packages = with pkgs; [
     ];
   };
+
   users.users.udh = {
     isNormalUser = true;
     description = "udh";
@@ -95,69 +145,96 @@
     shell = pkgs.zsh;
     packages = with pkgs; [
       alejandra
-      neofetch
+      bat
+      git
+      hyfetch
       netcat
+      scrot
+      stow
+      tmux
+      unzip
+      wdiff
+      zsh-autosuggestions
     ];
   };
+  # Enable sudo without having a user password
+  security.sudo.wheelNeedsPassword = false;
 
-  # Install firefox.
+  # Install core programs
+  programs.zsh.enable = true;
+  programs.mosh.enable = true;
   programs.firefox.enable = true;
-  # firefox kiosk
-  systemd.services.firefox = {
+  environment.systemPackages = with pkgs; [
+    barrier
+    htop
+    wget
+    vim
+  ];
+
+  # Enable Barrier
+  systemd.services.barrierc = {
     # this is the "node" in the systemd dependency graph that will run the service
-    wantedBy = [];
+    wantedBy = ["graphical.target"];
     # systemd service unit declarations involve specifying dependencies and order of execution of systemd nodes
-    after = ["graphical.target"];
-    description = "Start the firefox kiosk for reiko.";
+    after = ["network.target"];
+    description = "Start the brarrier client for Reiko.";
     serviceConfig = {
       # see systemd man pages for more information on the various options for "Type": "notify"
       # specifies that this is a service that waits for notification from its predecessor (declared in
       # `after=`) before starting
       Type = "simple";
       User = "reiko";
-      ExecStart = ''${pkgs.firefox}/bin/firefox --kiosk'';
-      ExecStop = ''${pkgs.procps}/bin/pkill firefox'';
+      ExecStart = ''${pkgs.barrier}/bin/barrierc -f --restart --disable-crypto 192.168.0.15'';
+      ExecStop = ''${pkgs.procps}/bin/pkill barrierc'';
     };
   };
-  
-  environment.systemPackages = with pkgs; [
-    # barrier
-    htop
-    wget
-    vim
-  ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # TODO: Kiosk Application
+  #systemd.services.firefox = {
+  #  # this is the "node" in the systemd dependency graph that will run the service
+  #  wantedBy = ["default.target"];
+  #  # systemd service unit declarations involve specifying dependencies and order of execution of systemd nodes
+  #  after = ["network.target"];
+  #  wants = ["display-manager.service"];
+  #  description = "Start the firefox kiosk for reiko.";
+  #  serviceConfig = {
+  #    # see systemd man pages for more information on the various options for "Type": "notify"
+  #    # specifies that this is a service that waits for notification from its predecessor (declared in
+  #    # `after=`) before starting
+  #    Type = "simple";
+  #    User = "reiko";
+  #    ExecStart = ''${pkgs.firefox}/bin/firefox --kiosk'';
+  #    ExecStop = ''${pkgs.procps}/bin/pkill firefox'';
+  #    Restart = "always";
+  #    RestartSec = "10";
+  #  };
+  #};
 
-  # Enable the OpenSSH daemon.
+  # Setup Remote Administration
   services.openssh = {
     enable = true;
+    settings.X11Forwarding = true;
     settings.PasswordAuthentication = false;
+    settings.PermitRootLogin = "no";
   };
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+  services.tor = {
+    enable = true;
+    openFirewall = true;
+    relay = {
+      enable = true;
+      role = "relay";
+      onionServices.remoteAdmin = {
+        version = 3;
+        map = [
+          {
+            port = 1234;
+            target = {
+              addr = "[::1]";
+              port = 22;
+            };
+          }
+        ];
+      };
+    };
+  };
 }
-
