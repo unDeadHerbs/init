aca2b57310c038923da93b1b1b5f8f5ca8329e70
Murray Fordyce <unDeadHerbs@gmail.com>
2025-07-10 21:15:20 -0500

    minor notes

---
+++ system_config/update_all.sh
@@ -0,0 +1,37 @@
+# network
+#   Update all mounts
+#   init_sync
+
+# windmills
+#  fix.sh
+
+# ptah
+#   guix pull -c 12
+#   guix package -c 12 --do-not-upgrade=telegram -u
+
+# sia
+#  sudo nixos-rebuild switch --upgrade
+
+# algernon
+#  sudo apt update
+#  #sudo apt upgrade
+#  U guix pull -c 4
+#  guix package -c 4 --keep-going -u && guix gc
+#  # Free up space on /boot?
+#  #sudo apt purge
+#  #sudo apt autoremove
+#  #sudo apt clean
+
+# work
+#  pacman -Syu
+
+# termux @ phone
+#  apt update
+#  apt upgrade
+
+# Chris@something
+#  # Probably just mount and sync init?
+
+# Wizard @ Mainrig
+#  # Check when the last update was and send a reminder
+#  # I want to encourage them performing the maintenance
